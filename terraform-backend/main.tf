2602ae1db94b0b4915430a24632ef4311b878ad7
Whykay243 <yinka.ajibola0011@gmail.com>
2025-05-24 11:28:43 -0400

    Update main.tf

--- terraform-backend/main.tf
+++ terraform-backend/main.tf
@@ -1,3 +1,7 @@
+provider "aws" {
+  region = var.region
+}
+
 resource "aws_s3_bucket" "terraform_state" {
   bucket = var.bucket_name
 
@@ -24,4 +28,17 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "terraform_state_e
       sse_algorithm = "AES256"
     }
   }
+}
+
+resource "aws_dynamodb_table" "terraform_locks" {
+  name         = var.dynamodb_table
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+
+  tags = var.tags
 }
